603f4b05e79b30449da1bd14447e5a087fccec8f
Evhesha <evgenmedvedskii@gmail.com>
2025-02-27 22:50:46 +0300

    last fixes in project.

--- Credit bank/obj/Debug/net9.0/Credit bank.AssemblyInfo.cs
+++ Credit bank/obj/Debug/net9.0/Credit bank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Credit bank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28b7dbceef8e4560c4fecf2b725d999f86e2c201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8650c7d71d84f52ce3c8a4e7fa4153dda0a4448a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Credit bank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Credit bank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
